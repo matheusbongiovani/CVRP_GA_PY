0478fdf6caafe331bf781158d010a555a6ac84d0
Matheus Bongiovani Sathler <matheusbongiovani@gmail.com>
2022-02-14 09:56:44 -0300

    Starting Chormosome.py

--- CVRPwithGA.py
+++ CVRPwithGA.py
@@ -1,5 +1,5 @@
 import sys
-import Node
+import Gene
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,7 +8,7 @@ import numpy as np
 # valores devem ser fornecidos pela entrada
 if len(sys.argv) != 3:
     print('----------------------ERROR----------------------')
-    print('Sintaxe: python3 "program" "instâncias" "solução" ')
+    print('Sintaxe: python3 "program.py" "instância.vrp" "solução" ')
     sys.exit('-------------------------------------------------')
 else:
     arg1 = sys.argv[1]
@@ -35,7 +35,7 @@ with open(arg1, mode='r', encoding='utf-8') as file:
             splited = linha.split()
             header_array.append(splited[2])
 
-        # trecho para pular linhas antes de armazenar a posição dos nós
+        # trecho para pular linhas da entrada que não será utilizado
         if num_linha >= 6 and num_linha < 9:
             num_linha += 1
             if num_linha > 8:
@@ -48,8 +48,9 @@ with open(arg1, mode='r', encoding='utf-8') as file:
                 demand_section_bool = True
 
             else:
-                splitXY = linha.split()
-                node = Node.Node(splitXY[1], splitXY[2])
+                split_id_XY = linha.split()
+                node = Gene.Gene(float(split_id_XY[1]), float(
+                    split_id_XY[2]), id=int(split_id_XY[0]))
                 array_of_nodes.append(node)
 
         # trecho para inserir a demanda de cada nó no vetor array_of_nodes
@@ -63,7 +64,7 @@ with open(arg1, mode='r', encoding='utf-8') as file:
                 array_of_nodes[i].z = splitZ[1]
                 i += 1
 
-
+# O Depot sempre será o 1º elemento e com demanda 0 (node: 0 <82,76,0>)
 n_numero_de_cidades = i
 
 # obter valor K, que representa o número de veículos dado pela entrada
@@ -73,16 +74,44 @@ k_numero_de_veiculos = k_numero_de_veiculos[1]
 Q_capacidade_maxima_veiculo = header_array[5]
 
 print(f'Número de cidades a serem atendidas: {n_numero_de_cidades}')
-print(f'Número de Veículos: {k_numero_de_veiculos}')
+print(f'Número de Veículos (número de rotas): {k_numero_de_veiculos}')
 print(f'Capacidade máxima do veículo: {Q_capacidade_maxima_veiculo}')
 
 
 def print_node_list(node_list):
-    node_index = 1
     for node in node_list:
-        print('node:' + str(node_index) + ' <' + str(node.getX()) +
-              ',' + str(node.getY()) + ',' + str(node.getZ()) + '>')
-        node_index += 1
+        print(node.__str__())
 
 
-# print_node_list(array_of_nodes)
+def x_values(node_list):
+    list_x = []
+    for node in node_list:
+        list_x.append(node.getX())
+    return list_x
+
+
+def y_values(node_list):
+    list_y = []
+    for node in node_list:
+        list_y.append(node.getY())
+    return list_y
+
+
+def total_demand(node_list):
+    total_demand = 0
+    for node in node_list:
+        total_demand += float(node.getZ())
+    return total_demand
+
+
+print_node_list(array_of_nodes)
+
+# print(total_demand(array_of_nodes))
+
+# plt.xkcd()  # deixar visual de quadrinho
+plt.grid(True)
+plt.scatter(x_values(array_of_nodes), y_values(
+    array_of_nodes), s=20, c='blue')
+plt.scatter(array_of_nodes[0].x, array_of_nodes[0].y, s=30, c='red')
+
+plt.show()
--- Gene.py
+++ Gene.py
@@ -1,8 +1,9 @@
-class Node(object):
-    def __init__(self, x=0, y=0, z=0):
+class Gene(object):
+    def __init__(self, x=0, y=0, z=0, id=0):
         self.x = x
         self.y = y
         self.z = z
+        self.id = id
 
     def distance_from_origin(self):
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
@@ -21,8 +22,11 @@ class Node(object):
     def getZ(self):
         return self.z
 
+    def getId(self):
+        return int(self.id)
+
     def __str__(self):
-        return '<' + str(self.getX()) + ',' + str(self.getY()) + ',' + str(self.getZ()) + '>'
+        return 'id:' + str(self.getId()) + ' (' + str(self.getX()) + ',' + str(self.getY()) + ')' + ' demand:' + str(self.getZ())
 
     def __eq__(self, other):
         return self.y == other.y and self.x == other.x
@@ -31,9 +35,8 @@ class Node(object):
 # c0 = Node(0, 0)
 # c1 = Node(-1, -1)
 # c2 = Node(2, 2)
+# print(c1.distance(c2))
+# print(c1.distance_from_origin())
 # print(c1)
 # print(c2)
 # print(c1 == c2)
-
-# print(c1.distance(c2))
-# print(c1.distance_from_origin())
